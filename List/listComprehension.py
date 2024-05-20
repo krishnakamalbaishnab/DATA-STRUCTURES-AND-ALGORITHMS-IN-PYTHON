5e0e8b0474fb96e7c881afb4545c576eebecbca1
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-05-20 10:38:47 +0100

    second commit of may 20

--- List/listComprehension.py
+++ List/listComprehension.py
@@ -38,6 +38,21 @@
 
 list10 = [-10,20,-45,23,25,-70]
 
-replaceList = [number if number > 0 else 0 for number in list10]  #the negativ number is replaceed with 0
+# replaceList = [number if number > 0 else 0 for number in list10]  #the negativ number is replaceed with 0
  
+# print(replaceList)
+
+# now let's do it by defining a functions
+
+def get_number(number):
+    return number if number > 0 else 'Negative Number'
+
+    # #let's break down the if statement above
+
+    # if number > 0:
+    #     return number
+    # else:
+    #     return 'Negative number'
+
+replaceList = [get_number(number) for number in list10]
 print(replaceList)
