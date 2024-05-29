94374633063ca4ab56118dce9eadc0ba368af088
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-05-29 06:10:47 +0100

    Find Average temperature of the from days

--- Project_List/Array/avgTemp.py
+++ Project_List/Array/avgTemp.py
@@ -4,12 +4,23 @@
 
 numberOfDays = int(input("How many days's temperature? :"))
 total = 0
+temp = []
 
-for i in range(1, numberOfDays+1):
+for i in range(numberOfDays+1):
     nextDay = int(input(str(i) + " Day's Highest Temmperatue? : "))
-    total +=nextDay
+    temp.append(nextDay)
+    total +=temp[i]
 
-avg = (total/numberOfDays)
+avg = round(total/numberOfDays,2)
+
+
+above = 0
+for i in temp:
+    if i > avg:
+        above  += 1
+
 
 print("The average temp of " +str(numberOfDays) + " days is : " + str(avg))
+
+print(str(above)  +  " days(s) above average")
 
